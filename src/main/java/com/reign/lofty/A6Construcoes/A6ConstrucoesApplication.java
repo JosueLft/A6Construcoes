44a0c4512ea48093ac855134b348bda4ddc148e4
Reign <josueflx1999@gmail.com>
2021-04-29 13:51:29 -0300

    --- Alteração do layout do projeto ---

---
+++ src/main/java/com/reign/lofty/A6Construcoes/A6ConstrucoesApplication.java
@@ -0,0 +1,13 @@
+package com.reign.lofty.A6Construcoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A6ConstrucoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A6ConstrucoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reign/lofty/A6Construcoes/A6ConstrucoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reign.lofty.A6Construcoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A6ConstrucoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
